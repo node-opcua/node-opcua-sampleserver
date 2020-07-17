4d649f43901313bf1f75cc0ad29187ae59675985
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-07-17 13:49:40 +0200

    update to latest node-opcua version

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 "use strict";
-
-const opcua = require("node-opcua");
+const {
+    OPCUAServer,
+    Variant,
+    DataType,
+    nodesets,
+    StatusCodes,
+    VariantArrayType,
+    ServerEngine
+} = require("node-opcua");
 const os = require("os");
 
 
-// Let create an instance of OPCUAServer
-const server = new opcua.OPCUAServer({
-    port: 26543,        // the port of the listening socket of the server
-    nodeset_filename: [
-        opcua.standard_nodeset_file
-    ]
-});
-
-// we can set the buildInfo
-server.buildInfo.productName = "MySampleServer1";
-server.buildInfo.buildNumber = "7658";
-server.buildInfo.buildDate = new Date(2015, 12, 25);
-
-
-function construct_my_address_space(server) {
+/**
+ * @param server {OPCUAServer} server
+ */
+function constructAddressSpace(server) {
 
     const addressSpace = server.engine.addressSpace;
     const namespace = addressSpace.getOwnNamespace();
@@ -32,7 +28,6 @@ function construct_my_address_space(server) {
     // the addVariableInFolder
     const variable1 = 10.0;
 
-
     server.nodeVariable1 = namespace.addVariable({
         componentOf: myDevice,
         nodeId: "s=Temperature",
@@ -42,31 +37,41 @@ function construct_my_address_space(server) {
             get: () => {
                 const t = new Date() / 10000.0;
                 const value = variable1 + 10.0 * Math.sin(t);
-                return new opcua.Variant({dataType: opcua.DataType.Double, value: value});
+                return new Variant({ dataType: DataType.Double, value: value });
             }
         }
     });
 
-
-    let variable2 = 10.0;
-
-    server.nodeVariable2 = namespace.addVariable({
+    const nodeVariable2 = namespace.addVariable({
         componentOf: myDevice,
         browseName: "MyVariable2",
+        dataType: "String",
+    });
+    nodeVariable2.setValueFromSource({
+        dataType: DataType.String,
+        value: "Learn Node-OPCUA ! Read https://leanpub.com/node-opcuabyexample"
+    });
+
+    const nodeVariable3 = namespace.addVariable({
+        componentOf: myDevice,
+        browseName: "MyVariable3",
         dataType: "Double",
-        value: {
-            get: () => new opcua.Variant({dataType: opcua.DataType.Double, value: variable2}),
-            set: (variant) => {
-                variable2 = parseFloat(variant.value);
-                return opcua.StatusCodes.Good;
-            }
-        }
+        arrayDimensions: [3],
+        accessLevel: "CurrentRead | CurrentWrite",
+        userAccessLevel: "CurrentRead | CurrentWrite",
+        valueRank: 1
+
+    });
+    nodeVariable3.setValueFromSource({
+        dataType: DataType.Double,
+        arrayType: VariantArrayType.Array,
+        value: [1.0, 2.0, 3.0]
     });
 
 
-    server.nodeVariable3 = namespace.addVariable({
+    const nodeVariable4 = namespace.addVariable({
         componentOf: myDevice,
-        nodeId: "b=1020ffab", // some opaque NodeId in namespace 4
+        nodeId: "b=1020ffab",
         browseName: "Percentage Memory Used",
         dataType: "Double",
         minimumSamplingInterval: 1000,
@@ -75,29 +80,53 @@ function construct_my_address_space(server) {
                 // const value = process.memoryUsage().heapUsed / 1000000;
                 const percentageMemUsed = 1.0 - (os.freemem() / os.totalmem());
                 const value = percentageMemUsed * 100;
-                return new opcua.Variant({dataType: opcua.DataType.Double, value: value});
+                return new Variant({ dataType: DataType.Double, value: value });
             }
         }
     });
 
+    // please 
 }
 
-// the server needs to be initialized first. During initialisation,
-// the server will construct its default namespace.
-server.initialize(function () {
+(async () => {
 
-    console.log("initialized");
+    try {
+        // Let create an instance of OPCUAServer
+        const server = new OPCUAServer({
+            port: 26543,        // the port of the listening socket of the server
 
-    // we can now extend the default name space with our variables
-    construct_my_address_space(server);
+            nodeset_filename: [
+                nodesets.standard,
+                nodesets.di
+            ],
+            buidIfo: {
+                productName: "Sample NodeOPCUA Server1",
 
-    // we can now start the server
-    server.start(function () {
+                buildNumber: "7658",
+                buildDate: new Date(2020, 6, 14)
+            }
+        });
+
+        await server.initialize();
+
+        constructAddressSpace(server);
+
+        // we can now start the server
+        await server.start();
+
         console.log("Server is now listening ... ( press CTRL+C to stop) ");
-        server.endpoints[0].endpointDescriptions().forEach(function (endpoint) {
+        server.endpoints[0].endpointDescriptions().forEach(function(endpoint) {
             console.log(endpoint.endpointUrl, endpoint.securityMode.toString(), endpoint.securityPolicyUri.toString());
         });
-    });
 
-});
 
+        process.on("SIGINT", async () => {
+            await server.shutdown();
+            console.log("terminated");
+
+        });
+    } catch (err) {
+        console.log(err);
+        process.exit(-1);
+    }
+})();
