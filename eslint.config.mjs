a29beb338d54c6588fed0c840c40ba916eb44002
Etienne Rossignon <etienne.rossignon@sterfive.com>
2024-12-08 16:17:52 +0100

    update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: { 
-      globals: globals.node 
-      
-    }
-
-  },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
